f79b1ff93055cf6d1d538180fd0d75de207c1842
tbcrafay <abdulrafaymehmood123@gmail.com>
2025-06-04 03:38:39 -0700

    project has been initialized

---
+++ qalbconnect/src/main/java/com/islamicapp/qalbconnect/QalbconnectApplication.java
@@ -0,0 +1,13 @@
+package com.islamicapp.qalbconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QalbconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QalbconnectApplication.class, args);
+	}
+
+}
---
+++ qalbconnect/src/test/java/com/islamicapp/qalbconnect/QalbconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.islamicapp.qalbconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QalbconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
